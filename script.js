e7f66d1ed88bc16e5426fc4c82dc9a39d27e62b7
Cássia <cascrisbern@gmail.com>
2023-01-10 18:13:08 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const students = [
+  {
+    name: 'John',
+    n1: 7,
+    n2: 9
+  },
+  {
+    name: 'Anna',
+    n1: 4,
+    n2: 2
+  },
+  {
+    name: 'Patrick',
+    n1: 10,
+    n2: 9
+  },
+  {
+    name: 'Helio',
+    n1: 8,
+    n2:3
+  }
+]
+
